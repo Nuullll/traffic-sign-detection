0140197130b708781a1f3c9bd9f3158a75b18c70
Nuullll <vfirst218@gmail.com>
2018-06-10 16:55:48 +0800

    Create run.m with description

---
+++ src/rcnn-sign-detection/run.m
@@ -0,0 +1,16 @@
+% traffic sign detection using R-CNN
+% https://www.mathworks.com/help/vision/examples/object-detection-using-deep-learning.html#d119e1338
+% 
+% R-CNN is an object detection framework,
+% which uses a CNN to classify image REGIONS within an image,
+% instead of using a sliding window.
+%
+% Transfer learning: 
+% Use a pretrained network (which has already learned a rich set of image
+% features) for the new task by fine-tuning the network.
+% A network is fine-tuned by making small adjustments to the weights 
+% such that the feature representations learned for the original task 
+% (general object detection) are slightly adjusted to support the new task 
+% (traffic sign detection).
+% Transfer learning reduces the number of the required training images 
+% and the time of the training process.
