5586ab0c4289d55df598d3831ed3d709e6ff94e2
Nuullll <vfirst218@gmail.com>
2018-06-11 17:56:58 +0800

    Train R-CNN

--- src/rcnn-sign-detection/train_rcnn_on_GTSDB.m
+++ src/rcnn-sign-detection/train_rcnn_on_GTSDB.m
@@ -3,3 +3,31 @@
 %% Load GTSDB dataset (900 images with ROI labels)
 gtsdbDir = fullfile(pwd, 'Origin\');
 gtsdbData = loadGTSDB2Table(gtsdbDir);
+nDataset = size(gtsdbData, 1);  % =1213 (some images may contain several ROI labels)
+
+
+%% Choose part of GTSDB dataset as training set
+nTrainset = 100;
+trainData = gtsdbData(randsample(nDataset, nTrainset), :);
+
+
+%% Train R-CNN Traffic Sign Detector
+doTraining = true;
+
+if doTraining
+    options = trainingOptions('sgdm', ...
+        'MiniBatchSize', 128, ...
+        'InitialLearnRate', 1e-3, ...
+        'LearnRateSchedule', 'piecewise', ...
+        'LearnRateDropFactor', 0.1, ...
+        'LearnRateDropPeriod', 100, ...
+        'MaxEpochs', 100, ...
+        'Verbose', true);
+    
+    rcnn = trainRCNNObjectDetector(trainData, cifar10Net, options, ...
+        'NegativeOverlapRange', [0 0.3], 'PositiveOverlapRange', [0.5 1]);
+    
+    save('rcnn-gtsdb.mat', 'rcnn');
+else
+    load('rcnn-gtsdb.mat', 'rcnn');
+end
