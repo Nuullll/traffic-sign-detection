80c3d63daaaa9d62d3be6993eb89e7d4964b81ce
Nuullll <vfirst218@gmail.com>
2018-06-11 17:39:36 +0800

    Load GTSDB dataset

--- src/rcnn-sign-detection/train_rcnn_on_GTSDB.m
+++ src/rcnn-sign-detection/train_rcnn_on_GTSDB.m
@@ -1,2 +1,5 @@
 % Fine-tune the pretrained CNN (on CIFAR-10) for traffic sign detection
 
+%% Load GTSDB dataset (900 images with ROI labels)
+gtsdbDir = fullfile(pwd, 'Origin\');
+gtsdbData = loadGTSDB2Table(gtsdbDir);
