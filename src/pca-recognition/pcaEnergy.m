7721c959e0b85e50a3a3684f587607c0897a92f6
Nuullll <vfirst218@gmail.com>
2018-06-24 16:15:02 +0800

    Add pca

---
+++ src/pca-recognition/pcaEnergy.m
@@ -0,0 +1,19 @@
+function [coeff, score, mu] = pcaEnergy(data, threshold)
+% Select principle components according to the energy threshold.
+% Input:    data        [nObservations, nFeatures]
+% Input:    threshold   energy threshold to select principle components
+% Output:   coeff       eigen matrix for the pca
+% Output:   score       low-d data representations
+% Output:   mu          mean of the sample data
+
+[coeff, score, latent, ~, ~, mu] = pca(data);
+
+% find threshold
+c = sum(latent);
+energy = cumsum(latent/c);
+nComponents = find(energy > threshold, 1, 'first');
+
+coeff = coeff(:, 1:nComponents);
+score = score(:, 1:nComponents);
+end
+
---
+++ src/pca-recognition/preprocess.m
@@ -0,0 +1,20 @@
+function [features] = preprocess(images)
+% Preprocessing the loaded images, as a feature extractor
+% Input:    images      [width,height,channels,nImages] 4-D uint8 matrix
+% Output:   features    [nImages, nFeatures]
+
+[w,h,~,nImages] = size(images);
+% gray intensity as feature
+nFeatures = w * h;
+features = zeros(nImages, nFeatures);
+
+% convert RGB to gray images
+for i = 1:nImages
+    image = images(:,:,:,i);
+    grayImg = rgb2gray(image);
+    % gray intensity as feature
+    features(i,:) = grayImg(:)';
+end
+
+end
+
